5d186f972db3975ddb9beac8aac74cfa2124cf78
Alexander Necheff <necheffa@necheff.net>
2019-11-09 11:14:23 -0500

    updated licensing terms

--- pgalloc.c
+++ pgalloc.c
@@ -1,11 +1,11 @@
 /*
 A "fast" fixed block size memory allocater
-Copyright (C) 2015,2018 Alexander Necheff
+Copyright (C) 2015,2018,2019 Alexander Necheff
 
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
+License as published by the Free Software Foundation;
+version 2.1 of the License
 
 This library is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
